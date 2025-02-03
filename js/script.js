cb0a51fd175fdf3e406dc2487ef0b86bb1c6dc84
akishajae <akishajae.angeles@gmail.com>
2025-02-03 13:48:59 +0100

    complete: bgColor, linkColor and show img

--- js/script.js
+++ js/script.js
@@ -39,39 +39,77 @@ amazonBtn.addEventListener("click", function () {
 
 // FACEBOOK
 
-let bgColorBtn = document.getElementById("bgColorBtn");
+const bgColorBtn = document.getElementById("bgColorBtn");
+const bgColorInput = document.getElementById("bgColorInput");
 bgColorBtn.addEventListener("click", changeColorBg);
 
 function changeColorBg() {
     chrome.scripting.executeScript({
         target: { tabId: tabId },
         func: (color) => {
-            console.log(color);
+            console.log("bgColor: ", color);
             document.body.style.backgroundColor = color;
+            // const elements = document.getElementsByClassName("_8esj _95k9 _8esf _8opv _8f3m _8ilg _8icx _8op_ _95ka");
+            // for (let element of elements) {
+            //     element.style.backgroundColor = color;
+            // }
         },
-        args: ["red"]
+        args: ["#9B1212"]
     });
 }
 
-let linkColorBtn = document.getElementById("linkColorBtn");
+const linkColorBtn = document.getElementById("linkColorBtn");
+const linkColorInput = document.getElementById("linkColorInput");
 linkColorBtn.addEventListener("click", changeColorLink);
 
 function changeColorLink() {
-
+    chrome.scripting.executeScript({
+        target: { tabId: tabId },
+        func: (color) => {
+            console.log("linkColor: ", color);
+            const links = document.getElementsByTagName("a");
+            for (let link of links) {
+                link.style.color = color;
+            }
+        },
+        args: [linkColorInput.value]
+    });
 }
 
-let imgBtn = document.getElementById("imgBtn");
+const imgBtn = document.getElementById("imgBtn");
 imgBtn.addEventListener("click", toggleImgView);
 
 function toggleImgView() {
-
+    chrome.scripting.executeScript({
+        target: { tabId: tabId },
+        func: () => {
+            const imgs = document.getElementsByTagName("img");
+            for (let img of imgs) {
+                img.style.display = img.style.display === 'none' ? 'block' : 'none';
+            }
+        }
+    });
 }
 
-let passwordBtn = document.getElementById("passwordBtn");
+const passwordBtn = document.getElementById("passwordBtn");
 passwordBtn.addEventListener("click", togglePasswordInput);
 
 function togglePasswordInput() {
+    chrome.scripting.executeScript({
+        target: { tabId: tabId },
+        func: () => {
+            console.log('test');
+            const passwordInputs = document.querySelectorAll('input[type="password"]');
+            passwordInputs.forEach(input => {
+                if (!input.hasAttribute('data-is-password')) {
+                    input.setAttribute('data-is-password', true);
+                }
 
+                input.getAttribute('data-is-password') === 'true' ? input.setAttribute('data-is-password', false) : input.setAttribute('data-is-password', true);
+                // input.getAttribute('data-is-password') === 'false' ? input.type = 'text' : input.type = 'password';
+            });
+        }
+    })
 }
 
 // AMAZON
